31bd0f0185d47d8b169b72198aca070beea4e471
Edmore Moyo <etmoyo@gmail.com>
2024-04-11 15:33:48 -0400

    renames variables

--- main.py
+++ main.py
@@ -33,9 +33,10 @@ for filename in source_files:
     df = pd.read_csv(f'{src}/{filename}')
     df['subject'] = extractSubject(df["file"].squeeze())
     dataframes.append(df)
-data = pd.concat(dataframes, axis=0)
+all_populations_unmelted = pd.concat(dataframes, axis=0)
+print(all_populations_unmelted)
 
-sorted_data = data.sort_values("file")
+sorted_data = all_populations_unmelted.sort_values("file")
 sorted_data.to_csv(f'{dest}/merged_results.csv')
 
 sns.set_theme(style="white")
@@ -43,24 +44,32 @@ sns.set_theme(style="white")
 hue_order = sorted_data['file'].to_list()
 subject_hue_order = sorted_data['subject'].to_list()
 
-df_melt = pd.melt(sorted_data, id_vars=['file', 'subject'], var_name='cell_type', value_name='cell_distribution')
-print(df_melt)
-tcell_sorted_data = sorted_data[["file","T cell","T cell CD4", "subject"]]
-df_melt_tcell = pd.melt(tcell_sorted_data, id_vars=['file', 'subject'], var_name='cell_type', value_name='cell_distribution')
-print(df_melt_tcell)
+all_populations_melted = pd.melt(sorted_data, id_vars=['file', 'subject'], var_name='cell_type', value_name='cell_distribution')
+print(all_populations_melted) # all populations melted
+
+# tcell_sorted_data = sorted_data[["file","T cell","T cell CD4", "subject"]]
+# df_melt_tcell = pd.melt(tcell_sorted_data, id_vars=['file', 'subject'], var_name='cell_type', value_name='cell_distribution')
+# print(df_melt_tcell)
 
 # plots
-raw_scatter = relplot(df_melt, "cell_type", "cell_distribution", "file", "scatter", hue_order)
-rotateXaxis(raw_scatter, 90).savefig(f'{dest}/raw_scatter.png')
+# raw_scatter = relplot(df_melt, "cell_type", "cell_distribution", "file", "scatter", hue_order)
+# rotateXaxis(raw_scatter, 90).savefig(f'{dest}/raw_scatter.png')
 
-scatter = relplot(df_melt, "cell_type", "cell_distribution", "subject", "scatter", subject_hue_order)
+scatter = relplot(all_populations_melted, "cell_type", "cell_distribution", "subject", "scatter", subject_hue_order)
 rotateXaxis(scatter, 90).savefig(f'{dest}/scatter.png')
 
-tcell_scatter = relplot(data, "T cell", "T cell CD4", "subject", "scatter", subject_hue_order)
+tcell_scatter = relplot(all_populations_unmelted, "T cell", "T cell CD4", "subject", "scatter", subject_hue_order)
 rotateXaxis(tcell_scatter, 90).savefig(f'{dest}/tcell_scatter.png')
 
-raw_tcell_scatter = relplot(data, "T cell", "T cell CD4", "file", "scatter", hue_order)
-rotateXaxis(raw_tcell_scatter, 90).savefig(f'{dest}/raw_tcell_scatter.png')
+# raw_tcell_scatter = relplot(data, "T cell", "T cell CD4", "file", "scatter", hue_order)
+# rotateXaxis(raw_tcell_scatter, 90).savefig(f'{dest}/raw_tcell_scatter.png')
+
+# pairplot = sns.pairplot(all_populations_unmelted, hue="subject")
+# rotateXaxis(pairplot, 90).savefig(f'{dest}/pairplot.png')
+
+# jointplot = sns.jointplot(data=all_populations_unmelted, x="T cell", y="T cell CD4", hue="subject", kind="resid")
+# jointplot.savefig(f'{dest}/jointplot.png')
+
 
 print("end of processing")
 
